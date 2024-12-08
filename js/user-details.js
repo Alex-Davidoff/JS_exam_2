7e4c2cabed13715bb7f93abdcbb692e2680a2650
Alex-Davidoff <alex.davidoff0009@gmail.com>
2024-12-08 13:23:36 +0200

    4

--- js/user-details.js
+++ js/user-details.js
@@ -5,7 +5,11 @@ fetch('https://jsonplaceholder.typicode.com/users/'+userid)
   .then((user) => writeUserDetailed(document.getElementById('userblock'), user)
   );
 
+const label_c = document.getElementById('posts_label');
+label_c.style.display = 'none';
+
 document.getElementById('btn_showUserPosts').onclick = function (){
+    label_c.style.display = 'block';
     const fetchurl = new URL('https://jsonplaceholder.typicode.com/users/'+userid+'/posts');
     document.getElementById('postsblock').replaceChildren();
     fetch(fetchurl.href)
